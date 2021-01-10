3da7fe9b7c817db87d1715f221e77a8752c8123c
jbhowsthisstuffwork <44230972+jbhowsthisstuffwork@users.noreply.github.com>
2021-01-10 08:24:59 -0600

    Collatz Upload v2
    Correctly earlier upload which was actually from this source.

--- Practice Projects/theCollatzSequence.py
+++ Practice Projects/theCollatzSequence.py
@@ -2,6 +2,8 @@
 # function on that number until the function returns the value 1.
 import time
 
+userInput = ''
+
 def collatz(number):
     global userInput
     evaluateNumber = number % 2
@@ -12,12 +14,17 @@ def collatz(number):
         userInput = 3 * number + 1
         print(number)
 
-userInput = int(input('Enter an integer other than 1.'))
+while userInput == '':
+    try:
+        userInput = int(input('Enter an integer other than 1.'))
+    except ValueError:
+        print('Not a valid integer. Please enter an integer other than 1.')
+
 while userInput != 1:
     collatz(userInput)
     time.sleep(.25) #Pause for .25 seconds
-
-
+else:
+    print(userInput)
 
         #if number = even, then print number // 2
         #if number = odd, then print 3 * number + 1
